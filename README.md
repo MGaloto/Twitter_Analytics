dd6ce3ac1c2916909eeb0f039f1f0eaef27ed6fd
MGaloto <maxigaloto@hotmail.com>
2023-06-22 17:57:28 -0300

    init repository

---
+++ README.md
@@ -0,0 +1,212 @@
+
+
+# Twitter Shiny App
+
+
+<p>
+<a href="https://rinterface.github.io/bs4Dash/" rel="nofollow"><img src="https://camo.githubusercontent.com/0c50278e279e08384b5a14b75112e4d65484105a9028e3ebe7c5192f438570a7/68747470733a2f2f72696e746572666163652e636f6d2f696e73742f696d616765732f627334446173682e737667" align="right" width="150" style="max-width: 100%;"></a>
+</p>
+
+
+
+### Contenido:
+<br>
+</br>
+
+- [**Introduccion**](https://github.com/MGaloto/twitter_analytics#introduccion)
+- [**Librerias**](https://github.com/MGaloto/twitter_analytics#librerias)
+- [**Estructura**](https://github.com/MGaloto/twitter_analytics#estructura)
+- [**Ejecucion**](https://github.com/MGaloto/twitter_analytics#ejecucion)
+- [**Conclusion**](https://github.com/MGaloto/twitter_analytics#conclusion)
+
+
+## Introduccion
+
+
+<div style="text-align: right" class="toc-box">
+ <a href="#top">Volver al Inicio</a>
+</div>
+
+<br>
+</br>
+
+Aplicacion de Twitter Analytics utilizando el framework [bs4Dash](https://rinterface.github.io/bs4Dash/). El Dashboard contiene la siguientes funcionalidades para interactuar con la API de Twitter y obtener Insights sobre alguna/s palabra/s en los ultimos Twitters para Argentina.
+
+
+* :large_blue_circle: Usuario que con mas frecuencia utilizo la palabra buscada en los ultimos Twitters.
+* :large_blue_circle: Que palabras acompañan la palabra buscada (Analisis de sentimiento).
+* :large_blue_circle: Grafico de barras para observar la frecuencia de las distintas palabras que acompañan a la consultada.
+
+
+<p align="center">
+  <img width="650" height="450" src="images/tw.gif">
+</p>
+
+
+## Librerias
+
+
+<div style="text-align: right" class="toc-box">
+ <a href="#top">Volver al Inicio</a>
+</div>
+
+<br>
+</br>
+
+Las siguientes librerias se utilizaron para la imagen:
+
+```json
+[
+  {
+    "package": "cpp11",
+    "version":"0.4.2"
+  },
+  {
+    "package": "bs4Dash",
+    "version": "2.2.1"
+  },
+  {
+    "package": "ggplot2",
+    "version": "3.3.5"
+  },
+  {
+    "package": "rtweet",
+    "version": "0.7.0"
+  },
+  {
+    "package": "tidyverse",
+    "version": "1.3.1"
+  },
+  {
+    "package": "tidytext",
+    "version": "0.3.2"
+  },
+  {
+    "package": "textdata",
+    "version": "0.4.1"
+  },
+  {
+    "package": "wordcloud2",
+    "version": "0.2.1"
+  },
+  {
+    "package": "lubridate",
+    "version": "1.7.10"
+  },
+  {
+    "package": "plotly",
+    "version": "4.10.1"
+  },
+  {
+    "package": "shinycssloaders",
+    "version": "1.0.0"
+  },
+  {
+    "package": "reactable",
+    "version": "0.2.3"
+  },
+  {
+    "package": "htmltools",
+    "version": "0.5.5"
+  },
+  {
+    "package": "waiter",
+    "version": "0.2.4"
+  },
+  {
+      "package": "rsconnect",
+      "version":"0.8.25"
+  },
+  {
+    "package": "languageserver",
+    "version":"0.3.13"
+  },
+  {
+    "package": "httpgd",
+    "version":"1.3.0"
+  },
+  {
+  "package": "httr",
+  "version":"1.4.2"
+  }
+]
+```
+
+
+## Estructura
+
+
+<div style="text-align: right" class="toc-box">
+ <a href="#top">Volver al Inicio</a>
+</div>
+
+<br>
+</br>
+
+
+``` shell
+. 
+├── docker-compose.yaml
+├── docker
+├── app.R
+└── images
+```
+
+- En `docker-compose.yaml` se encuentran configurados los puertos y la imagen que se va a utilizar para correr la app.
+- En `docker` se encuentra el Dockerfile y los packages para la app.
+- `app.R` es la Shiny App.
+- En`images` se encuentran las imagenes del repositorio.
+
+
+## Ejecucion
+
+
+<div style="text-align: right" class="toc-box">
+ <a href="#top">Volver al Inicio</a>
+</div>
+
+<br>
+</br>
+
+Para correr la app en un contenedor de Docker hay que utilizar el siguiente comando:
+
+``` shell
+docker-compose up -d
+```
+
+Este comando va a crear una instancia de una imagen (un contenedor) en el puerto que se encuentra configurado en el docker-compose.yaml. Si la imagen no esta descargada primero lo que va a hacer es descargarla y luego levantar el contenedor.
+
+Para detener y eliminar el contenedor hay que correr: 
+
+``` shell
+docker-compose down
+```
+
+
+## Conclusion
+
+
+<div style="text-align: right" class="toc-box">
+ <a href="#top">Volver al Inicio</a>
+</div>
+
+<br>
+</br>
+
+
+Como pudimos observar la app contiene distintas funcionalidades para hacer un analisis de palabras y sentimientos en Twitter para Argentina. 
+
+Utilizando la API de Twitter en R se pueden hacer los siguientes analisis:
+
+* Obtener tweets: Se puede buscar y extraer tweets utilizando palabras clave, hashtags, nombres de usuario y otras consultas avanzadas. Esto permite recopilar datos específicos de tweets para su análisis.
+
+* Analizar sentimientos: Se puede utilizar técnicas de procesamiento de lenguaje natural (NLP) para analizar el sentimiento de los tweets. Esto implica determinar si un tweet es positivo, negativo o neutro, lo que puede ser útil para el análisis de opiniones o la detección de tendencias.
+
+* Análisis de tendencias: Se puede identificar las tendencias actuales en Twitter, tanto a nivel global como a nivel local. Esto permite descubrir los temas populares en tiempo real y analizar las conversaciones en torno a ellos.
+
+* Interacciones de usuarios: Se puede obtener información sobre los seguidores, seguidos, menciones y retweets de usuarios específicos. Esto permite comprender las interacciones y relaciones entre los usuarios de Twitter.
+
+* Publicar tweets: Se puede utilizar la API para publicar tweets en tu cuenta de Twitter desde R. Esto es útil para automatizar la publicación de contenido o interactuar con los usuarios.
+
+* Recopilación de datos en tiempo real: Se puede utilizar la API de streaming de Twitter para recopilar datos en tiempo real sobre tweets que cumplan ciertos criterios. Esto es útil para el monitoreo continuo de eventos, seguimiento de menciones de marca o cualquier otra aplicación que requiera datos en tiempo real.
+
---
+++ app.R
@@ -0,0 +1,543 @@
+library(bs4Dash)
+library(ggplot2)
+library(rtweet)
+library(tidyverse)
+library(tidytext)
+library(textdata)
+library(wordcloud2)
+library(lubridate)
+library(plotly)
+library(shinycssloaders)
+library(reactable)
+library(htmltools)
+library(waiter)
+
+
+appname <- Sys.getenv('APPNAME_TW')
+key     <- Sys.getenv('KEY_TW')
+secret  <- Sys.getenv('SECRET_TW')
+token      = Sys.getenv('TOKEN_TW')
+token_secret = Sys.getenv('TOKEN_SECRET_TW')
+
+twitter_token <- create_token(
+  app = appname,
+  consumer_key= key,
+  consumer_secret = secret,
+  access_token= token,
+  access_secret= token_secret,
+  set_renv = TRUE)
+
+
+
+## StopWords en Español --> Palabras vacias
+
+sw = read.csv(
+  "https://raw.githubusercontent.com/stopwords-iso/stopwords-es/master/stopwords-es.txt",
+  stringsAsFactors = F, 
+  header = TRUE, 
+  col.names = c("word"),
+  encoding = 'UTF-8') %>% 
+  bind_rows(tibble(
+    word = c(
+      "https",
+      "t.co",
+      "rt",
+      "amp",
+      "rstats",
+      "gt"
+    )
+  )
+  )
+
+
+## Datos de sentimientos en español
+
+ss = read.csv(
+  "https://raw.githubusercontent.com/JoseCardonaFigueroa/sentiment-analysis-spanish/master/data/emotions.csv",
+  header = F,
+  col.names = c("word", "sentiment"),
+  stringsAsFactors = F, 
+  encoding = 'UTF-8')
+
+
+
+
+ss$sentiment[ss$sentiment == 'anger' ] = 'Enojo'
+ss$sentiment[ss$sentiment == 'disgust' ] = 'Asco'
+ss$sentiment[ss$sentiment == 'joy' ] = 'Alegria'
+ss$sentiment[ss$sentiment == 'surprise' ] = 'Sorprendido'
+ss$sentiment[ss$sentiment == 'fear' ] = 'Miedo'
+ss$sentiment[ss$sentiment == 'sadness' ] = 'Tristeza'
+
+
+
+new_words = function(input){
+  words_final = c()
+  vocales_acento = c("á", "é", "í", "ó", "ú")
+  vocales_sin_acento = c("a", "e", "i", "o", "u")
+  count = 0
+  for (vocal in vocales_acento) {
+    count = count + 1
+    words_final[count] = gsub(vocales_sin_acento[count], vocal, input)
+  }
+  return(words_final)
+}
+
+
+
+df <- data.frame(
+  valor1 = 120,
+  valor2 = 75,
+  valor3 = 42
+)
+
+# Crear la UI del dashboard
+
+# UI ----------------------------------------------------------------------
+
+
+
+ui <- dashboardPage(
+  options = F,
+  preloader = list(
+    html = tagList(spin_6(), "Loading Twitter Dashboard..."), color = "#384bb5"),
+  header = dashboardHeader(
+    skin = "dark",
+    fixed = T,
+    title = dashboardBrand(
+      title ="Twitter Words Dash",
+      image = "https://assets.stickpng.com/thumbs/580b57fcd9996e24bc43c53e.png")
+  ),
+  # dashboardSidebar ----------------------------------------------------------------------
+  sidebar = dashboardSidebar(
+    background = "lightgray",
+    background_dark = "darkgray",
+    skin = "light",
+    width = 500,
+    sidebarMenu(
+      menuItem(
+        text = "Principal",
+        tabName = "dashboard",
+        startExpanded = T,
+        icon = icon("home")
+      ),
+      selectInput(
+        inputId = "input1",
+        label = "Twitters",
+        choices = c(100,500,1000,1500,2000,2500,3000,4000), 
+        selected = 100),
+      textInput(
+        inputId = "input2",
+        label = "Word"
+      ),
+      actionButton(
+        inputId = "submit", 
+        "Query", 
+        status = "primary"),
+      menuItem(
+        text = "Word Cloud",
+        tabName = "wordcloud",
+        icon = icon("info-circle")),
+      menuItem(
+        text = "Acerca de",
+        tabName = "about",
+        icon = icon("info-circle"))
+      )
+  ),
+  body = dashboardBody(
+    tags$style(
+      type = 'text/css', 
+      '.rt-align-left {color: black!important; }',
+      '.rt-align-center {color: black!important; }',
+      '.ReactTable {color: black!important; }',
+      '.sidebar .shiny-input-container {padding: 0px 21px 0px 16px!important; }',
+      '.btn-primary {color: #fff!important; background-color: #5d6b7a!important; border-color: #007bff!important; box-shadow: none!important;}'
+    ),
+    tabItems(
+      tabItem(
+        # Sección Principal ----------------------------------------------------------------------
+        tabName = "dashboard",
+        h3("Twitter Words Dash"),
+        p("Dashboard sobre frecuencia de palabras claves en ", 
+          span("Twitter", style = "color: blue;"), " para Buenos Aires, Argentina."),
+        fluidRow(
+          bs4ValueBoxOutput("infobox_word"),
+          bs4ValueBoxOutput("infobox_user"),
+          bs4ValueBoxOutput("infobox_trend")
+        ),
+        
+        fluidRow(
+          column(
+            width = 12,
+            tabBox(
+              width = 12,
+              
+              tabPanel(
+                # UI freqplot ----------------------------------------------------------------------
+                title = "Freq Plot",
+                withSpinner(plotlyOutput("freqplot"),
+                            type = 1)
+              ),
+              tabPanel(
+                # UI table_output ----------------------------------------------------------------------
+                title = "Tweets Table",
+                withSpinner(
+                  reactable::reactableOutput("table_output"),
+                  type = 1
+                )
+              )
+              
+            )
+          )
+        )),
+      # UI wordcloud ----------------------------------------------------------------------
+      tabItem(
+        tabName = "wordcloud",
+        h1("WordCloud"),
+        p("Word Cloud sobre frecuencia de palabras claves en ", 
+          span("Twitter", style = "color: blue;"), " para Buenos Aires, Argentina."),
+        fluidRow(
+          column(
+            width = 8,
+            tabBox(
+              width = 12,
+              tabPanel(
+                title = "Word Cloud",
+                withSpinner(wordcloud2Output("wordcloud"),
+                            type = 1)
+              )
+            )
+          ),
+          column(
+            width = 4,
+            tabBox(
+              width = 12,
+              tabPanel(
+                title = "inputs",
+                sliderInput(inputId = "sizewordcloud",
+                            label = "Size",
+                            min = 0.1, 
+                            max = 0.5,
+                            value = 0.5, 
+                            step = 0.1,
+                            width = '200px'),
+                sliderInput(inputId = "rotatewordcloud",
+                            label = "Rotation",
+                            min = 0.1, 
+                            max = 2,
+                            value = 0.5, 
+                            step = 0.1,
+                            width = '200px'),
+                sliderInput(inputId = "gridwordcloud",
+                            label = "Grid",
+                            min = 10, 
+                            max = 30,
+                            value = 20, 
+                            step = 2,
+                            width = '200px'),
+                selectInput(inputId = "themewordcloud",
+                            label = "Theme",
+                            width = '200px',
+                            choices = c("Random dark" = "random-dark", 
+                                        "Random light" = "random-light"), 
+                            selected = "Random dark")
+              )
+            )
+          )
+        )
+      ),
+      tabItem(
+        tabName = "about",
+        h1("Acerca de:"),
+        fluidRow(
+          box(
+            title = strong("Dashboard sobre analisis de palabras en Twitter Argntina."), solidHeader = TRUE,
+            p("El Dashboard contiene la siguientes funcionalidades para interactuar con la API de Twitter y obtener Insights sobre alguna/s palabra/s en los ultimos Twitters para ", strong("Argentina"), "."),
+            p("Podemos ver las siguientes métricas:"),
+            tags$ol(
+              tags$li("Usuario que con mas frecuencia utilizo la palabra buscada en los ultimos Twitters."),
+              tags$li("Que palabras acompañan la palabra buscada (Analisis de sentimiento)."),
+              tags$li(" Grafico de barras para observar la frecuencia de las distintas palabras que acompañan a la consultada.")
+            ),
+            p("Los datos utilizados provienen de la API de Twitter:"),
+            tags$ul(
+              tags$li(tags$a(href="https://developer.twitter.com/es/developer-terms/policy", "Twitter Developers"))
+            ),
+            p("Packages:"),
+            tags$ul(
+              tags$li(tags$a(href="https://ggplot2.tidyverse.org/", "ggplot2")),
+              tags$li(tags$a(href="https://rinterface.github.io/bs4Dash/index.html", "bs4Dash")),
+              tags$li(tags$a(href="https://dplyr.tidyverse.org/", "dplyr")),
+              tags$li(tags$a(href="https://cran.r-project.org/web/packages/htmltools/index.html", "htmltools")),
+              tags$li(tags$a(href="https://plotly.com/r/", "plotly")),
+              tags$li(tags$a(href="https://glin.github.io/reactable/", "reactable")),
+              tags$li(tags$a(href="https://cran.r-project.org/web/packages/rtweet/index.html", "rtweet")),
+              tags$li(tags$a(href="https://cran.r-project.org/web/packages/tidytext/vignettes/tidytext.html", "tidytext")),
+              tags$li(tags$a(href="https://cran.r-project.org/web/packages/textdata/index.html", "textdata"))
+            )
+            
+          ),
+          box(
+            title = strong("Trabajo hecho por:"), solidHeader = TRUE,
+            h5("Galoto Maximiliano"),
+            tags$ul(
+              tags$li(tags$a(href="https://www.linkedin.com/in/maximilianogaloto", "Linkedin")),
+              tags$li(tags$a(href="https://github.com/MGaloto", "GitHub")))
+            
+          )
+        )
+        
+
+      )
+    )
+  )
+)
+
+# SV ----------------------------------------------------------------------
+server <- function(input, output,session) {
+
+  
+# reactiveValues----------------------------------------------------------------------
+  
+  rv <- reactiveValues()
+  
+
+  
+  
+  observeEvent(input$submit, {
+    req(input$input1)
+    req(input$input2)
+    
+
+    on.exit(
+      removeNotification(
+        showNotification(
+          paste0("Waiting.."), 
+          duration = as.numeric(input$input1) / 100, 
+          closeButton = TRUE,
+          type = "warning")
+        )
+      , add = TRUE)
+    
+# SV search_tweets ----------------------------------------------------------------------
+    
+    rv$tweet_words = search_tweets(
+      input$input2, 
+      n =  as.numeric(input$input1), 
+      include_rts = T, 
+      lang = "es",
+      geocode = "-34.603722,-58.381592,2000km") %>% 
+      mutate(
+        created_at = ymd_hms(
+          format(
+            created_at, tz = "America/Argentina/Buenos_Aires")
+        )
+      ) 
+    
+    rv$ranking = rv$tweet_words %>%
+      select(screen_name,
+             text) %>%
+      unnest_tokens(word, text) %>%
+      filter(!grepl("^\\d+", word)) %>%
+      mutate(word = chartr("áéíóúñÁÉÍÓÚÑ", "aeiounAEIOUN", word)) %>% 
+      anti_join(
+        sw %>% 
+          bind_rows(tibble(word = input$input2)) %>% 
+          bind_rows(tibble(word = new_words(input$input2))), by = "word") %>%
+      group_by(word) %>%
+      tally %>%
+      arrange(desc(n)) %>% 
+      head(50)
+    
+    rv$name_most_freq = rv$ranking[1,"word"]
+    rv$number_most_freq = rv$ranking[1,"n"]
+    
+    rv$tw_table = rv$tweet_words %>% select(created_at,
+                                            user_id,
+                                            media_url,
+                                            text,
+                                            screen_name,
+                                            status_id) %>% 
+      mutate(Date = as.Date(rtweet:::format_date(created_at)),
+             Time = paste0(format(created_at, "%H"),
+                           ":",
+                           format(created_at, "%M")),
+             Url = paste0("https://twitter.com/", 
+                          as.character(screen_name),
+                          "/status/",
+                          as.character(status_id)
+             ),
+             User = paste0("https://twitter.com/", 
+                           as.character(screen_name)
+             )
+      ) %>% select(Date, Time,Url, User, text, screen_name)
+    
+    
+    rv$ranking_user = rv$tw_table %>% 
+      group_by(User, screen_name) %>% 
+      count() %>% 
+      arrange(desc(n)) %>% 
+      select(User, screen_name) %>% 
+      head(1)
+    
+    rv$trend = get_trends("buenos aires") %>% 
+      arrange(desc(tweet_volume)) %>% 
+      select(trend, tweet_volume) %>% 
+      head(1)
+    
+    
+  }, ignoreNULL = FALSE)
+  
+  
+# SV wordcloud ----------------------------------------------------------------------
+  output$wordcloud <- renderWordcloud2({
+    req(rv$tweet_words)
+    
+    wordcloud2(rv$ranking,
+               minSize = 10, 
+               gridSize = as.numeric(input$gridwordcloud), 
+               size = as.numeric(input$sizewordcloud), 
+               rotateRatio =as.numeric(input$rotatewordcloud),
+               fontFamily = "Arial",
+               color=input$themewordcloud)
+  })
+# SV plotly ----------------------------------------------------------------------
+  
+  output$freqplot <- renderPlotly({
+    req(rv$tweet_words)
+    
+    ggplotly(ggplot(rv$ranking[order(-rv$ranking$n),] %>%
+                      head(15),
+                    aes(x = reorder(word, n),
+                        y = n,
+                        text = map(paste('<b>Metricas:</b>','<br>',
+                                         '<b>Word:</b>',word,'<br>',
+                                         '<b>Freq:</b>',n),HTML))) +
+               guides(fill = "none") +
+               geom_bar(stat = "identity",
+                        color = "black",
+                        alpha = 0.9,
+                        aes(fill = cut(n, breaks = 5)),
+                        size = 0.5) +
+               scale_fill_manual(
+                                 values = RColorBrewer::brewer.pal(9, "Blues")[5:9]) +
+               coord_flip() +
+               theme_dark() +
+               theme(panel.background = element_rect(fill = "white"),
+                     axis.text.y = element_text(angle = 0, hjust = 0.5)) +
+               labs(title = "Twitter Frequency Words",
+                    x = "",
+                    y = "Frequency"), tooltip = "text") %>%
+      config(displayModeBar = F)
+    
+    
+    
+  })
+  
+# SV table_output ----------------------------------------------------------------------
+  
+  output$table_output = reactable::renderReactable({
+    req(rv$tw_table)
+    
+    
+    reactable::reactable(
+      rv$tw_table,
+      columns = list(
+        Date = colDef(
+          name = "Date",
+          align = "center",
+          minWidth = 60
+        ),
+        Time = colDef(
+          name = "Time",
+          align = "center",
+          minWidth = 60
+        ),
+        screen_name = colDef(
+          name = "User",
+          cell = function(screen_name) {
+            htmltools::tags$a(
+              href = paste0(
+                "https://twitter.com/", as.character(screen_name)
+              ),
+              target = "_blank", paste0("@",screen_name))
+          },
+          minWidth = 60
+        ),
+        text = colDef(
+          name = "Tweet",
+          align = "left",
+          minWidth = 120
+        ),
+        Url = colDef(
+          name = "Url Tweet",
+          cell = function(Url) {
+            htmltools::tags$a(
+              href = as.character(Url),
+              target = "_blank", as.character(Url))
+          }
+        ),
+        User = colDef(
+          name = "User Link",
+          cell = function(User) {
+            htmltools::tags$a(
+              href = as.character(User),
+              target = "_blank", as.character(User))
+          }
+        )
+      ),
+      searchable = TRUE,
+      striped = TRUE,
+      defaultPageSize = 8)
+    
+    
+  })
+  
+  # value box ----------------------------------------------------------------------
+  
+  output$infobox_word <- renderbs4ValueBox({
+
+    bs4ValueBox(
+      value = paste0(if (is.null(rv$name_most_freq)) {"Insertar Word y Ejecutar Query"} else {paste0("Palabra: ",as.character(rv$name_most_freq))}),
+      subtitle = paste0(if (is.null(rv$number_most_freq)) {""} else {paste0("Se encuentra ",as.character(rv$number_most_freq), " veces dentro de los ",as.character(input$input1)," Twitters.")}),
+      icon = icon("table"),
+      color = "primary",
+      width = NULL,
+      footer = div("Mas frecuente")
+    )
+  })
+  
+  
+  output$infobox_user <- renderbs4ValueBox({
+
+    bs4ValueBox(
+      value = paste0(if (is.null(rv$ranking_user)) {"Insertar Word y Ejecutar Query"} else {paste0("User: ",as.character(rv$ranking_user[1,"screen_name"]))}),
+      subtitle = paste0(if (is.null(rv$ranking_user)) {""} else {paste0("Usuario con mas Twitts que contienen la palabra ",as.character(rv$name_most_freq))}),
+      icon = icon("user-circle"),
+      color = "indigo",
+      width = 3,
+      gradient = T,
+      footer = div("User")
+    )
+    
+  })
+    
+    output$infobox_trend <- renderbs4ValueBox({
+
+      bs4ValueBox(
+        value = paste0(if (is.null(rv$trend)) {"Insertar Word y Ejecutar Query"} else {paste0("Trend en Argentina: ",as.character(rv$trend %>% pull(trend)))}),
+        subtitle = paste0(if (is.null(rv$trend)) {""} else {paste0("Total de Twitts que contienen el Trend: ",as.character(rv$trend %>% pull(tweet_volume)))}),
+        icon = icon("hourglass-half"),
+        color = "fuchsia",
+        width = NULL,
+        footer = div("Trend Argentina")
+      )
+      
+    
+  })
+  
+  
+  
+}
+
+shinyApp(ui = ui, server = server)
